a77c8f1264cd5b11480d62c781dabd16c5a761d7
jirkah31 <jirkah.31@seznam.cz>
2023-05-10 20:07:44 +0200

    update package.json

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 import './Login.scss'
 
 function Login () {
-	return (
-		<div className='login'>
-			<div className='border'>
-				<h2>Log In</h2>
-				<form method='post'>
-					<label htmlFor='email'>Email</label>
-					<input type='email' name='email' placeholder='me@example.com' required />
+  return (
+    <div className='login'>
+      <div className='border'>
+        <h2>Log In</h2>
+        <form method='post'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email' placeholder='me@example.com' required />
 
-					<label htmlFor='password'>Password</label>
-					<input type='password' name='password' placeholder='**********' required />
-					<button type='submit'>Log In</button>
-				</form>
-			</div>
-		</div>
-	)
+          <label htmlFor='password'>Password</label>
+          <input type='password' name='password' placeholder='**********' required />
+          <button type='submit'>Log In</button>
+        </form>
+      </div>
+    </div>
+  )
 }
-
+ 
 export default Login;
